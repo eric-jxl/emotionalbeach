b386279893de6cca977e764d0df3a95edd273b58
Eric <799050481@qq.com>
2025-08-28 11:07:53 +0800

    修改镜像文件
    Signed-off-by: Eric <799050481@qq.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 # 如果 CONFIG_PATH 环境变量被设置，则使用它，否则使用默认值
 if [ -z "$CONFIG_PATH" ]; then
-  CONFIG_PATH="/app/config/.env"
+  CONFIG_PATH="/app/config/config.yaml"
+fi
+
+# 判断文件是否存在
+if [ ! -f "$CONFIG_PATH" ]; then
+  echo "\x1b[31m错误：配置文件 $CONFIG_PATH 不存在\x1b[0m"
+  exit 1
 fi
 
 exec /app/emnotonalBeach -e "$CONFIG_PATH"
