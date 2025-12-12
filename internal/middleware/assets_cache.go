5f013d3bc066a6bd870cacf63435907a628e41bf
Eric <799050481@qq.com>
2025-12-12 17:15:24 +0800

    修改缓存过期时间

--- internal/middleware/assets_cache.go
+++ internal/middleware/assets_cache.go
@@ -11,9 +11,9 @@ import (
 func AssetsCacheMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if strings.HasPrefix(c.Request.URL.Path, "/assets/") {
-			// 设置缓存时间为 1 小时 (你可以根据需要调整)
+			// 设置缓存时间为 7天 (你可以根据需要调整)
 			// Cache-Control 是现代浏览器推荐的主要缓存控制头
-			c.Header("Cache-Control", "public, max-age=3600") // 3600 秒 = 1 小时
+			c.Header("Cache-Control", "public, max-age=604800")
 			// Expires 是较老的头，但为了兼容性也可以加上
 			expiresTime := time.Now().Add(7 * 24 * time.Hour)
 			c.Header("Expires", expiresTime.UTC().Format(http.TimeFormat))
